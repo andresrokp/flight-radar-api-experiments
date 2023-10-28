2e356b56105ee47c1c294b79e9d4f856881ce417
andresrokp <afrokap@gmail.com>
2023-10-27 19:02:49 -0500

    chore: printing data as a "beauty" table
    using tabulate library
    iterate over data array
    get properties an save in new variables
    push new vars in a list (array)
    build tabulate object and print

--- directFrApiQuery.py
+++ directFrApiQuery.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from tabulate import tabulate
 
 headers = {
     "accept-encoding": "gzip, br",
@@ -19,7 +21,48 @@ try:
     response = requests.get(url,headers=headers)
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        print(json.dumps(data,indent=4))
+        out_file = 'arrivals.json'
+        with open(out_file,'w') as arrivals_file:
+            json.dump(data, arrivals_file, indent=4)
+        
+        # Extract relevant information for arrivals
+        arrivals_data = data["result"]["response"]["airport"]["pluginData"]["schedule"]["arrivals"]["data"]
+        # Define a bucket list to store data as rows
+        table_data = []
+
+        # Iterate through the arrivals and extract relevant information
+        for arrival in arrivals_data:
+
+            #get macro properties
+            flight = arrival.get("flight", {})
+            identification = flight.get("identification", {})
+            status = flight.get("status", {})
+            aircraft = flight.get("aircraft", {})
+            owner = flight.get("owner", {})
+            airline = flight.get("airline", {})
+            airport = flight.get("airport", {})
+            time = flight.get("time", {})
+
+            # Extracting relevant sub properties
+            flight_id = identification.get("id", "N/A")
+            flight_number = identification.get("number", {}).get("default", "N/A")
+            airline_name = airline.get("name", "N/A")
+            origin = airport.get("origin", {}).get("name", "N/A")
+            destination = airport.get("destination", {}).get("name", "N/A")
+            departure_time = time.get("real", {}).get("departure", "N/A")
+            arrival_time = time.get("real", {}).get("arrival", "N/A")
+            status_text = status.get("text", "N/A")
+
+            # Append the extracted data as a row in the bucket list
+            table_data.append([flight_id, flight_number, airline_name, origin, destination, departure_time, arrival_time, status_text])
+
+        # Headers in normal write style
+        headers = ["ID", "Flight Number", "Airline", "Origin", "Destination", "Departure Time", "Arrival Time", "Status"]
+
+        # Print the formatted table
+        print(tabulate(table_data, headers, tablefmt="fancy_grid"))
+
     else:
         print("BAD")
 except requests.exceptions.RequestException as e:
